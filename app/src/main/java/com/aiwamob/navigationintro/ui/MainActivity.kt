9ca1c2d5b71cf948d09f6179daa05404215a5fe7
leonel ka <waboleonel@gmail.com>
2020-05-04 23:17:25 +0100

    Hooking up navigation up

--- app/src/main/java/com/aiwamob/navigationintro/ui/MainActivity.kt
+++ app/src/main/java/com/aiwamob/navigationintro/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.aiwamob.navigationintro.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.aiwamob.navigationintro.R
 import com.aiwamob.navigationintro.databinding.ActivityMainBinding
 
@@ -12,8 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = DataBindingUtil.setContentView(this,
+        mainBinding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         )
+
+        val navController = this.findNavController(R.id.navigationHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navigationHostFragment)
+        return navController.navigateUp()
     }
 }
